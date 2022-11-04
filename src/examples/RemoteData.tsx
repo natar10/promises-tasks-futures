474ebf4ef69c1c9986d84e93d0e7394b7596058f
Nat Rocha <nata_r10@hotmail.com>
2022-11-04 15:30:41 -0500

    remote data

--- src/examples/RemoteData.tsx
+++ src/examples/RemoteData.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
+import * as RD from '@devexperts/remote-data-ts'
 import * as T from 'fp-ts/lib/Task'
-import { GithubUser } from '../components/types'
+import * as TE from 'fp-ts/lib/TaskEither'
+import { GithubRepo } from '../components/types'
 import {
   Alert,
   Box,
   Button,
-  ColumnLayout,
   Container,
 } from '@cloudscape-design/components'
 import { Layout } from '../components/Layout'
@@ -14,62 +14,62 @@ import { pipe } from 'fp-ts/lib/function'
 import { GITHUB_API } from '../utils/constants'
 
 export const RemoteData = () => {
-  const [promiseUser, setPromiseUser] = useState<GithubUser>()
-  const [taskUser, setTaskUser] = useState<GithubUser>()
+  const [response, setResponse] = useState<
+    RD.RemoteData<Error, Array<GithubRepo>>
+  >(RD.initial)
 
-  //This is a normal promise definition
-  const normalPromise = fetch(`${GITHUB_API}/natar10`)
-    .then(response => response.json())
-    .then(data => setPromiseUser(data))
+  const request = TE.tryCatch<Error, Response>(
+    () => fetch(`${GITHUB_API}/natar10/repos`),
+    () => new Error('The request failed')
+  )
 
-  //This is a Task definition
-  const task: T.Task<GithubUser> = () =>
-    fetch(`${GITHUB_API}/stackbuilders`)
-      .then(response => response.json())
-      .then(data => data)
-
-  const showTaskData = () => pipe(task, T.map(setTaskUser))()
+  const runRemoteData = () => {
+    if (RD.isInitial(response)) {
+      const artificialDelay = TE.fromTask(
+        T.delay(300)(T.fromIO(() => setResponse(RD.pending)))
+      )
+      pipe(
+        request,
+        TE.apFirstW(artificialDelay),
+        TE.chainW(a =>
+          TE.tryCatch(
+            () => a.json() as Promise<Array<GithubRepo>>,
+            _ => new Error('Invalid JSON')
+          )
+        ),
+        TE.match(
+          e => setResponse(RD.failure(e as Error)),
+          s => setResponse(RD.success(s))
+        )
+      )()
+    }
+  }
 
   return (
-    <Layout title="Laziness" subtitle="Lets see the difference">
-      <ColumnLayout columns={2}>
-        <Container>
-          <h2>Promise</h2>
-          <p>The promise is called as soon as we load</p>
-          <Alert
-            type="warning"
-            visible
-            header="The promise is called as soon as we load"
-          />
-          {promiseUser && (
-            <ul>
-              <li>{promiseUser.name}</li>
-              <li>{promiseUser.created_at}</li>
-              <li>{promiseUser.id}</li>
-            </ul>
+    <Layout title="Error Handling" subtitle="Lets see the difference">
+      <Container>
+        <h2>Task Either Success</h2>
+        <Button variant="primary" onClick={runRemoteData}>
+          Run Remote Data
+        </Button>
+        <Box margin={{ top: 'xl' }}>
+          {pipe(
+            response,
+            RD.fold(
+              () => <Alert type="info" header="Not called yet" />,
+              () => <Alert type="warning" header="Loading..." />,
+              e => <Alert type="error" header={e.message} />,
+              res => (
+                <ul>
+                  {res.map(repo => (
+                    <li key={repo.id}>{repo.name}</li>
+                  ))}
+                </ul>
+              )
+            )
           )}
-        </Container>
-        <Container>
-          <h2>fp-ts Task</h2>
-          <p>The task is called only when we decide to do it</p>
-          <Button onClick={showTaskData}>Run Task</Button>
-          {taskUser && (
-            <Box padding={{ top: 'l' }}>
-              <Alert
-                type="success"
-                visible
-                header="The task was called only when we decided to call it"
-              />
-              <ul>
-                <li>{taskUser.name}</li>
-                <li>{taskUser.created_at}</li>
-                <li>{taskUser.id}</li>
-              </ul>
-            </Box>
-          )}
-        </Container>
-      </ColumnLayout>
+        </Box>
+      </Container>
     </Layout>
   )
-  return <h1>Lazy</h1>
 }
